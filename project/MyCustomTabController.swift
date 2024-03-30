d08ac509471b97d72481b97a90798cf9666a85ae
Kanapiya <n1ght.d1ck1ns@gmail.com>
2024-03-30 12:31:38 +0500

    Add tab bar animation

---
+++ project/MyCustomTabController.swift
@@ -0,0 +1,29 @@
+//
+//  MyCustomTabController.swift
+//  project
+//
+//  Created by Канапия Базарбаев on 30.03.2024.
+//
+
+import UIKit
+
+class MyCustomTabController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
