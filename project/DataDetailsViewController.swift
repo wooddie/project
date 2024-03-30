2eaeea1a3730acf0d2c789fef14053e72bd4d2f9
Kanapiya <n1ght.d1ck1ns@gmail.com>
2024-03-30 11:46:01 +0500

    Initial Commit

---
+++ project/DataDetailsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DataDetailsViewController.swift
+//  project
+//
+//  Created by Канапия Базарбаев on 30.03.2024.
+//
+
+import UIKit
+
+class DataDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
