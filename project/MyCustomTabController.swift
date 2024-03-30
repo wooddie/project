922027da805a5e777db3b0fd5db9859325455760
Kanapiya <n1ght.d1ck1ns@gmail.com>
2024-03-30 12:32:02 +0500

    add animation

--- project/MyCustomTabController.swift
+++ project/MyCustomTabController.swift
@@ -8,22 +8,47 @@
 import UIKit
 
 class MyCustomTabController: UITabBarController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.delegate = self
+        
         // Do any additional setup after loading the view.
     }
-    
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension UITabBarController: UITabBarControllerDelegate {
+    public func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+        guard let fromView = tabBarController.selectedViewController?.view, let toView = viewController.view else {
+            return false
+        }
+        
+        // Определяем направление перехода
+        let isMovingLeft = tabBarController.selectedIndex < (tabBarController.viewControllers?.firstIndex(of: viewController) ?? 0)
+        
+        // Устанавливаем начальную позицию для нового экрана
+        if isMovingLeft {
+            toView.frame = CGRect(x: tabBarController.view.frame.size.width, y: 0, width: tabBarController.view.frame.size.width, height: tabBarController.view.frame.size.height)
+        } else {
+            toView.frame = CGRect(x: -tabBarController.view.frame.size.width, y: 0, width: tabBarController.view.frame.size.width, height: tabBarController.view.frame.size.height)
+        }
+        
+        // Добавляем новый экран поверх текущего экрана
+        tabBarController.view.addSubview(toView)
+        
+        // Анимированный переход между экранами
+        UIView.animate(withDuration: 0.3, animations: {
+            if isMovingLeft {
+                fromView.frame = CGRect(x: -tabBarController.view.frame.size.width, y: 0, width: tabBarController.view.frame.size.width, height: tabBarController.view.frame.size.height)
+            } else {
+                fromView.frame = CGRect(x: tabBarController.view.frame.size.width, y: 0, width: tabBarController.view.frame.size.width, height: tabBarController.view.frame.size.height)
+            }
+            toView.frame = CGRect(x: 0, y: 0, width: tabBarController.view.frame.size.width, height: tabBarController.view.frame.size.height)
+        }) { (finished) in
+            // Удаляем предыдущий экран после завершения анимации
+            fromView.removeFromSuperview()
+        }
+        
+        return true
     }
-    */
-
 }
